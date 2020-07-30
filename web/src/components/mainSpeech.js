74fe4c56c5da510882071cc4251a427d27efa286
amiralitaheri <amiralitaheri64@gmail.com>
2020-07-30 13:46:11 +0430

    added Action buttons

--- web/src/components/mainSpeech.js
+++ web/src/components/mainSpeech.js
@@ -3,15 +3,19 @@ import React from 'react'
 import styles from './mainSpeech.module.css'
 
 const MainSpeech = () => (
+  <div>
+    <ul className={styles.mainSpeech}>
+      <li>Fast learner</li>
+      <li>React front-end developer</li>
+      <li>Professional google searcher</li>
+      <li>A Confused man in tech world</li>
+      <li>Computer engineering student</li>
+    </ul>
+    <div style={{height:'2em'}}></div>
+    <Link className={styles.primaryAction} to='/blog/'>Blog</Link>
+    <a className={styles.secondaryAction} href='#works'>Works</a>
+  </div>
 
-  <ul className={styles.mainSpeech}>
-    <li>Fast learner</li>
-    <li>React front-end developer</li>
-    <li>Professional google searcher</li>
-    <li>A Confused man in tech world</li>
-    <li>Computer engineering student</li>
-  </ul>
-  
 )
 
 export default MainSpeech
