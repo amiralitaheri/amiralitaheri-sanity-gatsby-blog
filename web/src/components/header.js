0b3c359a05e9d267f05ce46dae1fdfcbc84f798e
amiralitaheri <amiralitaheri64@gmail.com>
2020-08-24 16:18:56 +0430

    fixed header link

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -5,7 +5,7 @@ import {cn} from '../lib/helpers'
 
 import styles from './header.module.css'
 
-const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
+const Header = ({onHideNav, onShowNav, showNav, siteTitle, isMainPage}) => (
   <div className={styles.root}>
     <div className={styles.wrapper}>
       <div className={styles.branding}>
@@ -15,23 +15,35 @@ const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
         <Icon symbol='hamburger'/>
       </button>
-
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>
-          <li>
-            <a href='#works'>Works</a>
-          </li>
-          <li>
-            <a href='#skills'>Skills</a>
-          </li>
-          <li>
-            <a href='#about'>About</a>
-          </li>
-          <li>
-            <Link to='/blog/'>Blog</Link>
-          </li>
-        </ul>
-      </nav>
+      {
+        (typeof window !== 'undefined' ? window.location.pathname === '/' : true) ?
+          <nav className={cn(styles.nav, showNav && styles.showNav)}>
+            <ul>
+              <li>
+                <a href='#works'>Works</a>
+              </li>
+              <li>
+                <a href='#skills'>Skills</a>
+              </li>
+              <li>
+                <a href='#about'>About</a>
+              </li>
+              <li>
+                <Link to='/blog/'>Blog</Link>
+              </li>
+            </ul>
+          </nav> :
+          <nav className={cn(styles.nav, showNav && styles.showNav)}>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/blog/'>Blog</Link>
+              </li>
+            </ul>
+          </nav>
+      }
     </div>
   </div>
 )
