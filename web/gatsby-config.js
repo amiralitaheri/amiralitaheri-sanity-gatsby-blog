5447832b67c080e24acb09d1cfe6b07cb4dc1a46
amiralitaheri <amiralitaheri64@gmail.com>
2020-08-26 21:02:19 +0430

    added sitemap plugin

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -29,5 +29,36 @@ module.exports = {
         ignore: [`**/\.*`], // ignore files starting with a dot
       },
     },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      // options: {
+      //   query: `
+      //   {
+      //     wp {
+      //       generalSettings {
+      //         siteUrl
+      //       }
+      //     }
+      //
+      //     allSitePage {
+      //       nodes {
+      //         path
+      //       }
+      //     }
+      // }`,
+      //   resolveSiteUrl: ({site, allSitePage}) => {
+      //     //Alternatively, you may also pass in an environment variable (or any location) at the beginning of your `gatsby-config.js`.
+      //     return site.wp.generalSettings.siteUrl
+      //   },
+      //   serialize: ({ site, allSitePage }) =>
+      //     allSitePage.nodes.map(node => {
+      //       return {
+      //         url: `${site.wp.generalSettings.siteUrl}${node.path}`,
+      //         changefreq: `daily`,
+      //         priority: 0.7,
+      //       }
+      //     })
+      // }
+    },
   ]
 }
