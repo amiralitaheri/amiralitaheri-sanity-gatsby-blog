7d5bdf1ce454984bf520fe994c336a9e326a918c
amiralitaheri <amiralitaheri64@gmail.com>
2020-09-08 17:26:25 +0430

    changed figure hover cursor and increased image max width

--- web/src/components/Figure.js
+++ web/src/components/Figure.js
@@ -2,17 +2,17 @@ import React from 'react'
 import Img from 'gatsby-image'
 import {getFluidGatsbyImage} from 'gatsby-source-sanity'
 import clientConfig from '../../client-config'
-
+import styles from './Figure.module.css';
 export default ({node}) => {
   if (!node || !node.asset || !node.asset._id) { return null }
   const fluidProps = getFluidGatsbyImage(
     node.asset._id,
-    {maxWidth: 675},
+    {maxWidth: 1080},
     clientConfig.sanity
   )
   return (
     <figure>
-      <Img fluid={fluidProps} alt={node.alt} />
+      <Img fluid={fluidProps} alt={node.alt} className={styles.image}/>
       <figcaption>{node.caption}</figcaption>
     </figure>
   )
